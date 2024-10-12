88f0d40df0430734138736713317ceb36528dac1
Maksim Balakin <balakinmax41@gmail.com>
2024-10-12 14:17:07 +1200

    edited model to remove redundant fields present in AbstractUser()

--- ecommerce/users/models.py
+++ ecommerce/users/models.py
@@ -5,11 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
 
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
     address = models.TextField()
-    is_admin = models.BooleanField(default=False)
-
+    
     def __str__(self):
         return self.email
